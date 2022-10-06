f2682ffbad8a6eb2416d0f01081626b54e2353e9
TheOfficialLOE <max.shady1611@gmail.com>
2022-10-07 00:58:27 +0330

    Change naming convention

--- src/Main.ts
+++ src/Main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './AppModule';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+bootstrap();
