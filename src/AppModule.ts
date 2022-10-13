a5c65afe02e14844a3c725f5db4654e407be3e48
TheOfficialLOE <max.shady1611@gmail.com>
2022-10-13 20:44:07 +0330

    Add identity and access module

--- src/AppModule.ts
+++ src/AppModule.ts
@@ -1,7 +1,11 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
+import { IdentityAndAccessModule } from "./modules/identity-and-access/IdentityAndAccessModule";
 
 @Module({
-  imports: [MongooseModule.forRoot("mongodb://localhost/morad-chat")],
+  imports: [
+    MongooseModule.forRoot("mongodb://localhost/morad-chat"),
+    IdentityAndAccessModule,
+  ],
 })
 export class AppModule {}
