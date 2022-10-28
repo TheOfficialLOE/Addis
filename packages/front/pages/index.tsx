415b135d4f088028000fa457ff3274c43612e72d
TheOfficialLOE <max.shady1611@gmail.com>
2022-10-28 16:09:04 +0330

    Add btn attribute to li element

--- packages/front/pages/index.tsx
+++ packages/front/pages/index.tsx
@@ -1,18 +1,18 @@
 const Index = () => {
   return <div className="h-screen">
     <div className="grid grid-cols-[min-content_auto] grid-flow-dense h-full">
-      <ul className="w-96 bg-neutral">
-        <li className="flex flex-row hover:transition-all hover:bg-neutral-focus p-4">
-          <div className="w-12 h-12 flex justify-center items-center rounded-xl my-auto bg-pink-600">
+      <ul className="w-96 bg-neutral p-4">
+        <li className="btn justify-start w-full h-20">
+          <div className="w-12 h-12 avatar justify-center items-center rounded-xl bg-pink-600">
             <p className="text-white font-bold">J</p>
           </div>
-          <div className="ml-4 flex flex-col">
+          <div className="ml-4 flex flex-col items-start">
             <p className="text-white font-bold">Jason</p>
-            <p className="text-sm">Jason: What up buddy?</p>
+            <p className="text-sm mt-2">Jason: What up buddy?</p>
           </div>
-          <div className="flex flex-col items-end ml-auto">
+          <div className="flex flex-col items-end items-end grow">
             <p>8:52 PM</p>
-            <div className="badge bg-primary">1</div>
+            <div className="badge badge-primary mt-2">1500</div>
           </div>
         </li>
       </ul>
