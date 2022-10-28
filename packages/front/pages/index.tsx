132225c54b521a01cef5fc457c716b70a5abb3f7
TheOfficialLOE <max.shady1611@gmail.com>
2022-10-28 13:55:22 +0330

    Add specific width to the left column

--- packages/front/pages/index.tsx
+++ packages/front/pages/index.tsx
@@ -1,14 +1,14 @@
 const Index = () => {
   return <div className="h-screen">
-    <div className="grid grid-cols-3 h-full">
-      <ul className="bg-neutral">
-        <li className="flex flex-row transition hover:transition-all hover:bg-neutral-focus active:bg-primary p-4">
+    <div className="grid grid-cols-[min-content_auto] grid-flow-dense h-full">
+      <ul className="w-96 bg-neutral">
+        <li className="flex flex-row hover:transition-all hover:bg-neutral-focus p-4">
           <div className="w-12 h-12 flex justify-center items-center rounded-xl my-auto bg-pink-600">
-            <p className="text-white font-bold">P</p>
+            <p className="text-white font-bold">J</p>
           </div>
           <div className="ml-4 flex flex-col">
-            <p className="text-white font-bold">Pedaret</p>
-            <p className="text-sm">Kir mikhay?</p>
+            <p className="text-white font-bold">Jason</p>
+            <p className="text-sm">Jason: What up buddy?</p>
           </div>
           <div className="flex flex-col items-end ml-auto">
             <p>8:52 PM</p>
@@ -16,7 +16,7 @@ const Index = () => {
           </div>
         </li>
       </ul>
-      <div className="col-span-2">Messages</div>
+      <div className="">Messages</div>
     </div>
   </div>
 };
