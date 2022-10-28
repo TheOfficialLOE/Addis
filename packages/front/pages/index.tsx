a38f2989e75e2e6232d3e5159bfbd4f4c4be6e81
TheOfficialLOE <max.shady1611@gmail.com>
2022-10-28 11:34:24 +0330

    Basic look and feel of index route + change daisy theme

--- packages/front/pages/index.tsx
+++ packages/front/pages/index.tsx
@@ -1,5 +1,24 @@
 const Index = () => {
-  return <p>Signed</p>
+  return <div className="h-screen">
+    <div className="grid grid-cols-3 h-full">
+      <ul className="bg-neutral">
+        <li className="flex flex-row transition hover:transition-all hover:bg-neutral-focus active:bg-primary p-4">
+          <div className="w-12 h-12 flex justify-center items-center rounded-xl my-auto bg-pink-600">
+            <p className="text-white font-bold">P</p>
+          </div>
+          <div className="ml-4 flex flex-col">
+            <p className="text-white font-bold">Pedaret</p>
+            <p className="text-sm">Kir mikhay?</p>
+          </div>
+          <div className="flex flex-col items-end ml-auto">
+            <p>8:52 PM</p>
+            <div className="badge bg-primary">1</div>
+          </div>
+        </li>
+      </ul>
+      <div className="col-span-2">Messages</div>
+    </div>
+  </div>
 };
 
 export default Index;
--- packages/front/tailwind.config.js
+++ packages/front/tailwind.config.js
@@ -17,6 +17,6 @@ module.exports = {
     require("daisyui")
   ],
   daisyui: {
-    themes: ["forest"]
+    themes: ["night"]
   }
 };
